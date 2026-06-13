0cdd4356d133f585f83fb6248b554504cbe5f100
surya809526 <swarajdubey605@gmail.com>
2026-06-13 11:47:53 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (req,res)=>{
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT,()=>{
+    console.log("Desktop running on port "+PORT);
+});
